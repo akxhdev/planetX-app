aad297af581026d71b497b92efa4b52890e5cbf2
akxhdev <akxhdev@gmail.com>
2022-11-05 16:41:39 +0530

    modify: auth form submit button

--- lib/pages/auth/submit_button.dart
+++ lib/pages/auth/submit_button.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:planetx/pages/auth/auth_page_provider.dart';
+import 'package:planetx/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class SubmitButton extends StatelessWidget {
   const SubmitButton({super.key});
@@ -8,19 +11,30 @@ class SubmitButton extends StatelessWidget {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(8),
-      child: ElevatedButton(
-        onPressed: null,
-        style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.all(16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24),
+      child: Consumer<AuthPageProvider>(
+        builder: (context, provider, child) => ElevatedButton(
+          onPressed: () {
+            if (provider.isLoginForm) {
+              var authRequest = provider.generateAuthRequest();
+
+              if (authRequest != null) {
+                Provider.of<AuthProvider>(context, listen: false)
+                    .login(authRequest.alienId, authRequest.password);
+              }
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            textStyle: Theme.of(context)
+                .textTheme
+                .titleMedium
+                ?.copyWith(fontWeight: FontWeight.bold),
           ),
-          textStyle: Theme.of(context)
-              .textTheme
-              .titleMedium
-              ?.copyWith(fontWeight: FontWeight.bold),
+          child: Text(provider.isLoginForm ? "Login" : "Sign Up"),
         ),
-        child: const Text("Login"),
       ),
     );
   }
